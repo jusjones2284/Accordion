956e4b6c15ef2c125cf51e05a26e52cd375879e2
Justin Jones <jus.jones@yahoo.com>
2023-06-08 19:08:18 -0400

    created up new components and added state

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
+import { useState } from 'react';
+import data from './data'
+
 const App = () => {
+ const [questions, setQuestions] = useState(data);
+ console.log(questions[1]);
+
   return <h2>Accordion Starter</h2>;
 };
 export default App;
