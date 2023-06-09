312b96d90d928cb4b6704b2c82d97704b3fc67ae
Justin Jones <jus.jones@yahoo.com>
2023-06-08 20:28:41 -0400

    update

--- src/SingleQuestion.jsx
+++ src/SingleQuestion.jsx
@@ -2,13 +2,18 @@ import React, { useState } from 'react'
 import {AiOutlineMinus, AiOutlinePlus} from 'react-icons/ai'
 
 const SingleQuestion = ({title, info}) => {
-  const [showInfo, setShowInfo] = useState(true)
+  const [showInfo, setShowInfo] = useState(false)
 
   return (
 
    <article className='question'>
        <header>
            <h5>{title}</h5>
+           <button 
+           className='question-btn' 
+           onClick={()=> setShowInfo(!showInfo)}>
+               {showInfo? <AiOutlinePlus />: <AiOutlineMinus />}
+           </button>
         </header>
            {showInfo && <p>{info}</p>}   
    </article>
